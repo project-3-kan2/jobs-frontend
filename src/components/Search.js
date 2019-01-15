9d12892595aa8a4312a5872443ae398153c52f06
nadaabdulkarem <asghar.nada@gmail.com>
2019-01-15 12:13:02 +0300

    link search, searchResult and JobDetails component (team work)

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,14 @@
 import React , { Component } from 'react';
-import SearchResult from './SearchResult'
+import SearchResult from './SearchResult';
+import JobDetails from './JobDetails';
 
 class Search extends Component {
     constructor() {
         super();
         this.state = {
             searchTerm: '',
-            results: []
+            results: [],
+            selectedJob: null
         }
     }
 
@@ -21,7 +23,10 @@ class Search extends Component {
 
     handleSumbit(event) {
         event.preventDefault();
-        const url1 = `https://jobs.github.com/positions.json?title=${this.state.searchTerm}&search=node`
+        this.setState({
+            selectedJob: null
+        })
+        const url1 = `https://jobs.github.com/positions.json?description=${this.state.searchTerm}&search=node`
         const url2 = `https://authenticjobs.com/api/?api_key=e2da8aacbfce53f1e1b9559409a51691&format=json&method=aj.jobs.search&keywords=${this.state.searchTerm}`
 
         fetch(url1)
@@ -46,13 +51,14 @@ class Search extends Component {
     }
 
     handleGithubData(data) {
+        const img = 'https://image.freepik.com/free-icon/blocking-symbol_318-40339.jpg'
         const parsedData = data.map ( job => {
             return{
                 title: job.title,
                 description: job.description.replace(/<\/?[^>]+(>|$)/g, ""),
                 location: job.location,
                 company: job.company,
-                company_logo: job.company_logo,
+                company_logo: job.company_logo === null ? img : job.company_logo,
                 url: job.url
             }
         })
@@ -73,7 +79,6 @@ class Search extends Component {
                 company: listing.company.name,
                 company_logo: img,
                 url: listing.company.apply_url
-                //listing.company.logo,
             }
         })
         console.log('handleAuthenticData', parsedListings);
@@ -87,7 +92,14 @@ class Search extends Component {
 
     renderResults() {
         return this.state.results.map((job, index) => {
-            return <SearchResult key={index} job={job}/>
+            return <SearchResult key={index} job={job} setSelectedJob={this.setSelectedJob.bind(this)}/>
+        })
+    }
+
+    // changr the state of the selectedjob when user click on the job
+    setSelectedJob(activeJob) {
+        this.setState({
+            selectedJob: activeJob
         })
     }
 
@@ -103,7 +115,7 @@ class Search extends Component {
                         <input type="text" onChange={this.handleChange.bind(this)}/>
                         <button><img src="https://i.imgur.com/WX7bym4.png" alt="search"/></button>
                     </form>
-                    {this.renderResults()}
+                    {this.state.selectedJob === null ? this.renderResults() : <JobDetails selectedJob={this.state.selectedJob}/> }
                 </div>
             </div>
         )
--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const SearchResult = (props) => {
+const SearchResult = (props) => {    
     return(
         <div className="search-resualt">
-            <div>
+            <div onClick={() => props.setSelectedJob(props.job)}>
                 <h2>{props.job.title}</h2>
                 <p>Company: {props.job.company}</p>
                 <p>Location: {props.job.location}</p>
