2bae44677ab3ac52408ebdaa0aae4b96187c713a
aishah21 <aishah-almaghrabi@hotmail.com>
2019-01-15 23:41:43 +0300

    userform and userprofile

---
+++ src/components/UserProfile.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const UserProfile = props => {
+  return (
+    <div className="userProfile">
+      <div>
+        <h4>Username: {props.user.username}</h4>
+        <h4>First Name: {props.user.firstname}</h4>
+        <h4>Last Name: {props.user.lastname}</h4>
+        <h4>Email: {props.user.email}</h4>
+        <h4>Phone: {props.user.phone}</h4>
+      </div>
+      <div>
+        <div className="edit">
+
+          <button>Edit</button>
+
+        </div>
+      </div>
+    </div>
+
+
+  );
+}
+
+export default UserProfile;
+
