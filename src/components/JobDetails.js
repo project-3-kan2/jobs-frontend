254cfbb930bd5b5ea664ddc80971dc9680ea96d2
nadaabdulkarem <asghar.nada@gmail.com>
2019-01-19 13:11:29 +0300

    remove unneeded pakage

--- src/components/JobDetails.js
+++ src/components/JobDetails.js
@@ -3,21 +3,21 @@ import React from "react";
 const JobDetails = props => {
   return (
     <div>
-      {/* <divnclassName="back" onClick={() => {props.setCurrentJob(null);}}>back</div> */}
+      <div className="back" onClick={() => { props.setCurrentJob(null) }}>back</div>
       <div className="job-details">
-          <img src={props.selectedJob.company_logo} alt={props.selectedJob.company_name}/>
-            <h2>{props.selectedJob.title}</h2>
-            <h4>Company: {props.selectedJob.company_name}</h4>
-            <h4>Location: {props.selectedJob.job_location}</h4>
-            <p>Description: {props.selectedJob.description}</p>  
-          
-            { props.userInfo === undefined? '' :
-              <button onClick={() => window.open(`${props.selectedJob.job_url}`, "_blank")}>Apply</button>
-            }
-            <button onClick={() => props.handleSaveJob(props.selectedJob)}>Save</button>
-        </div>
+        <img src={props.selectedJob.company_logo} alt={props.selectedJob.company_name} />
+        <h2>{props.selectedJob.title}</h2>
+        <h4>Company: {props.selectedJob.company_name}</h4>
+        <h4>Location: {props.selectedJob.job_location}</h4>
+        <p>Description: {props.selectedJob.description}</p>
+
+        {props.userInfo === undefined ? '' :
+          <button onClick={() => window.open(`${props.selectedJob.job_url}`, "_blank")}>Apply</button>
+        }
+        <button onClick={() => props.handleSaveJob(props.selectedJob)}>Save</button>
       </div>
-   
+    </div>
+
   );
 }
 
